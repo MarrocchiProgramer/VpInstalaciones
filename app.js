f2f4a7a97c0ee35d18e9f04202fc322dc299e5e8
MarrocchiProgrammer <tomasmarrocchi@gmail.com>
2023-07-12 16:44:19 -0300

    12-07-23

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function redirigirWhatsApp() {
+  var numero = "+5491166143033"; // Reemplaza con el número de teléfono de destino
+  
+  var enlaceWhatsApp = "https://wa.me/" + numero;
+  window.open(enlaceWhatsApp, "_blank");
+}
