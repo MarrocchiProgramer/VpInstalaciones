248f8042daa5d26dc906d431a35208a8562ad3cb
MarrocchiProgrammer <tomasmarrocchi@gmail.com>
2023-07-14 19:38:25 -0300

    14-07-23 3.0

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 
 function redirigirWhatsApp() {
-  var numero = "+5491166143033"; // Reemplaza con el número de teléfono de destino
-  
-  var enlaceWhatsApp = "https://wa.me/" + numero;
-  window.open(enlaceWhatsApp, "_blank");
+    var numero = "+5491166143033"; // Reemplaza con el número de teléfono de destino
+
+    var enlaceWhatsApp = "https://wa.me/" + numero;
+    window.open(enlaceWhatsApp, "_blank");
 }
 
 if (window.innerWidth <= 767) {
     var elemento = document.querySelector('.elemento');
     elemento.setAttribute('data-aos-delay', '0');
     elemento.setAttribute('data-aos', '0');
-  }
+}
+
+var contactoCelular = document.querySelector('.contacto-celular');
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function () {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        contactoCelular.classList.remove('fixed');
+    } else {
+        contactoCelular.classList.add('fixed');
+    }
+
+    lastScrollTop = scrollTop;
+});
